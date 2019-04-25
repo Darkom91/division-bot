d1a9e1de34ed3e7e4f63ac1da29420d1b23de7dc
Darkom91 <49963183+Darkom91@users.noreply.github.com>
2019-04-25 21:37:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ client.on("ready", () =>{
     console.log("je suis prêt")
     client.user.setGame("Ban des Trolleurs")
 });
+
+bot.on('message', function (message) {
+    if (message.content === '!Collection') {
+      message.reply('Hey, pour avoir la Collection, il te suffit de rejoindre ce lien:                                          https://steamcommunity.com/sharedfiles/filedetails/?id=1721373787                                                                                                                                                                      Bonne journé.')
+    }
+})
+
+bot.on('message', function (message) {
+    if (message.content === '!Forum') {
+      message.reply('Hey, pour avoir rejoindre le Forum, il te suffit de rejoindre ce lien:                                          https://divisionrp.forumchitchat.com/                                                                                                                                                                                             Bonne journé.')
+    }
+})
