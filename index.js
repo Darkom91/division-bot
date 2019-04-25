e065ac78ad701babae666f1a10cdf4a9aa774a32
Darkom91 <49963183+Darkom91@users.noreply.github.com>
2019-04-25 20:44:15 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+var prefix = "dv"
+
+client.login("NTcwMjY5MjYzMjI3NjUwMDc4.XL-PXQ.30CfGjfoUyvN8zEGtF9vQPQ6gHs")
+
+client.on("ready", () =>{
+    console.log("je suis prêt")
+    client.user.setGame("Ban des Trolleurs")
+});
