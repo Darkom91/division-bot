da62cb7c246860e838ff3dcd27318113b98cb8f8
Darkom91 <49963183+Darkom91@users.noreply.github.com>
2019-04-25 21:56:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
-const client = new Discord.Client()
+const bot = new Discord.Client();
 
 var prefix = "dv"
 
-client.login("NTcwMjY5MjYzMjI3NjUwMDc4.XL-PXQ.30CfGjfoUyvN8zEGtF9vQPQ6gHs")
+bot.login("NTcwMjY5MjYzMjI3NjUwMDc4.XL-PXQ.30CfGjfoUyvN8zEGtF9vQPQ6gHs")
 
-client.on("ready", () =>{
+bot.on("ready", () =>{
     console.log("je suis prêt")
-    client.user.setGame("Ban des Trolleurs")
-});
-
-bot.on('message', function (message) {
-    if (message.content === '!Collection') {
-      message.reply('Hey, pour avoir la Collection, il te suffit de rejoindre ce lien:                                          https://steamcommunity.com/sharedfiles/filedetails/?id=1721373787                                                                                                                                                                      Bonne journé.')
-    }
+    bot.user.setGame("Ban des Trolleurs")
 })
 
 bot.on('message', function (message) {
-    if (message.content === '!Forum') {
-      message.reply('Hey, pour avoir rejoindre le Forum, il te suffit de rejoindre ce lien:                                          https://divisionrp.forumchitchat.com/                                                                                                                                                                                             Bonne journé.')
-    }
+  if (message.content === '!Collection') {
+    message.reply('Hey, pour avoir la Collection, il te suffit de rejoindre ce lien:                                          https://steamcommunity.com/sharedfiles/filedetails/?id=1721373787                                                                                                                                                                      Bonne journé.')
+  }
+})
+
+bot.on('message', function (message) {
+  if (message.content === '!Forum') {
+    message.reply('Hey, pour avoir rejoindre le Forum, il te suffit de rejoindre ce lien:                                          https://divisionrp.forumchitchat.com/                                                                                                                                                                                             Bonne journé.')
+  }
 })
